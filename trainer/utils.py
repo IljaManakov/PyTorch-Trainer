9b7eb3f9e7e471a3258e25e12f25fe91fb1a2bfc
Ilja Manakov <ilja.manakov@med.uni-muenchen.de>
2019-06-21 15:14:29 +0200

    added to_numpy and IntervalBased

--- trainer/utils.py
+++ trainer/utils.py
@@ -1,3 +1,7 @@
+from collections import Sequence
+from functools import wraps
+
+import torch as pt
 import torch.nn as nn
 import torch.nn.init as init
 
@@ -73,3 +77,47 @@ def weight_init(m):
 
 def identity(x):
     return x
+
+
+def to_numpy(sample):
+    """
+    convert individual and sequences of Tensors to numpy, while leaving strings, integers and floats unchanged
+    :param sample: individual or sequence of Tensors, ints, floats or strings
+    :return: same as sample but all Tensors are converted to numpy.ndarray
+    """
+    if isinstance(sample, int) or isinstance(sample, float) or isinstance(sample, str):
+        return sample
+    elif isinstance(sample, pt.Tensor):
+        return sample.detach().cpu().numpy()
+    elif isinstance(sample, tuple) or isinstance(sample, list):
+        return [to_numpy(s) for s in sample]
+    elif isinstance(sample, Sequence):
+        collection = sample.__class__
+        return collection(*[to_numpy(s) for s in sample])
+
+
+class IntervalBased(object):
+    """
+    used for decorating functions.
+    the function will then only be executed at set intervals
+    """
+    def __init__(self, interval):
+        """
+        :param interval: interval at which the function will be called
+        """
+        self.interval = interval
+        self.counter = 0
+
+    def __call__(self, func):
+        """
+        wraps function so that it is executed only at set interval
+        :param func: function to wrap
+        :return: wrapped function
+        """
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            self.counter += 1
+            if self.counter % self.interval == 0:
+                return func(*args, **kwargs)
+
+        return wrapped
