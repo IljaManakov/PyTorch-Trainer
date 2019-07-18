3115bfbcc7372a9b73c7f04b1be67ea8bab0c55f
IljaManakov <ilja.manakov@med.uni-muenchen.de>
2019-07-18 09:45:50 +0200

    updated to new factory method

--- examples/dummy_training.py
+++ examples/dummy_training.py
@@ -2,7 +2,7 @@ from trainer import Trainer, events
 from examples.dummy_components import MNIST
 from torch.utils.data import DataLoader
 
-trainer = Trainer.from_config('dummy_config.py')
+trainer = Trainer.from_config_file('/home/ilja/Documents/Promotion/Project_Helpers/trainer/examples/dummy_config.py')
 
 trainer.register_event_handler(events.EACH_STEP, trainer.test_on_sample, interval=10,
                                sample=next(iter(trainer.dataloader)), model=trainer.model, criterion=trainer.criterion)
