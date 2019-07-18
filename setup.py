19683e250896209bbc85b90f56ef62c2f330f9fa
IljaManakov <ilja.manakov@med.uni-muenchen.de>
2019-07-18 09:47:14 +0200

    fixed bug; output of monitored methods was not stored properly

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pt-trainer",
-    version="0.0.6",
+    version="0.0.7",
     author="Ilja Manakov",
     author_email="ilja.manakov@gmx.de",
     description="A toolkit for training pytorch models",
