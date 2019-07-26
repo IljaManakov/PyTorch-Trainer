8ef896cd58cbe1d16e9b208bf5514cf5dee03e28
Ilja Manakov <ilja.manakov@med.uni-muenchen.de>
2019-07-26 10:59:44 +0200

    implemented event handlers for saving and training loss

---
+++ trainer/handlers.py
@@ -0,0 +1,31 @@
+from trainer.events import AbstractEventHandler
+from trainer.mixins import SaveMixin
+
+
+class EventSave(AbstractEventHandler):
+    """
+    Adapter for the save method that constructs checkpoint name from epoch and step
+    and passes logdir as directory
+    """
+
+    def __init__(self, *, func=SaveMixin.save, name=None, interval=None):
+        super().__init__(func, name=name, interval=interval)
+
+    def __call__(self, trainer, key=None, loss=None, step=None, epoch=None):
+        checkpoint_name = f'epoch-{epoch+1}' if step is None else f'epoch-{epoch}_step-{step}'
+        self.func(trainer, directory=trainer.logdir, checkpoint_name=checkpoint_name)
+        return checkpoint_name
+
+
+class TrainingLoss(AbstractEventHandler):
+    """
+    Adapter for recording training loss. Simply takes in the loss at each step and returns it.
+    If registered as an event handler and monitored the loss will be saved to storage
+    """
+
+    def __init__(self, func=lambda x: x, name='training-loss', interval=None):
+        super().__init__(func, name=name, interval=interval)
+
+    def __call__(self, trainer, key=None, loss=None, step=None, epoch=None):
+        return self.func(loss)
+
