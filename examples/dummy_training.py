495ff9295fc2c1d248eed0e71b83021787f540ec
Ilja Manakov <ilja.manakov@med.uni-muenchen.de>
2019-07-26 10:57:00 +0200

    updated example

--- examples/dummy_training.py
+++ examples/dummy_training.py
@@ -1,18 +1,15 @@
 from trainer import Trainer, events, Config
+from trainer.handlers import EventSave
 from examples.dummy_components import MNIST
 from torch.utils.data import DataLoader
 
 config = Config.from_file('dummy_config.py')
 trainer = Trainer.from_config(config, altered=True)  # altered is set as a test to see if Config.dump works
-sample = trainer._transform(next(iter(trainer.dataloader)))
+sample = next(iter(trainer.dataloader))
 validation_loader = DataLoader(MNIST('validation.pt'))
 
-trainer.register_event_handler(events.EACH_STEP, trainer.test_on_sample, interval=10,
-                               sample=sample, model=trainer.model, criterion=trainer.criterion)
-trainer.register_event_handler(events.EACH_EPOCH, trainer.validate,
-                               dataloader=validation_loader, forward_pass=trainer.forward_pass)
-trainer.register_event_handler(events.EACH_EPOCH, trainer.save,
-                               directory=trainer.logdir)
-trainer.monitor(name='criterion')
+trainer.register_event_handler(events.EACH_STEP, trainer, name='inference', interval=10, sample=sample)
+trainer.register_event_handler(events.EACH_EPOCH, trainer.validate, dataloader=validation_loader)
+trainer.register_event_handler(events.EACH_EPOCH, EventSave())
 
 trainer.train(n_epochs=1)
