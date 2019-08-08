8dfee26eba9f51688b24dda600b822cbf3b7b769
IljaManakov <ilja.manakov@med.uni-muenchen.de>
2019-08-08 15:34:33 +0200

    updated examples to demostrate rgetattr and rsetattr

--- examples/dummy_training.py
+++ examples/dummy_training.py
@@ -8,8 +8,11 @@ trainer = Trainer.from_config(config, altered=True)  # altered is set as a test 
 sample = next(iter(trainer.dataloader))
 validation_loader = DataLoader(MNIST('validation.pt'))
 
+# register handlers for events (see in trainer.events and trainer.handlers)
 trainer.register_event_handler(events.EACH_STEP, trainer, name='inference', interval=10, sample=sample)
 trainer.register_event_handler(events.EACH_EPOCH, trainer.validate, dataloader=validation_loader)
 trainer.register_event_handler(events.EACH_EPOCH, EventSave())
 
-trainer.train(n_epochs=1)
+trainer.monitor(name='model.activation')  # register monitoring for parts of the trainer. Return values will be stored
+
+trainer.train(n_steps=4)
