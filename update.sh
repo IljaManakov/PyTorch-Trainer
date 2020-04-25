b1bf5e6c42505c6454acc421943e896f8eed5e71
IljaManakov <32897953+IljaManakov@users.noreply.github.com>
2020-04-25 19:58:58 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python setup.py sdist bdist_wheel
-python -m twine upload dist/*
-rm -r /home/kazuki/Documents/Coding_Projects/PyTorch-Trainer/dist
-rm -r /home/kazuki/Documents/Coding_Projects/PyTorch-Trainer/build
-rm -r /home/kazuki/Documents/Coding_Projects/PyTorch-Trainer/pt_trainer.egg-info
